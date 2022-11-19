2b182ba3a095479819e486927bc8bdd1ea7be9bb
Seulgigi <slki1205@naver.com>
2022-11-19 19:48:59 +0900

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -101,7 +101,14 @@ CORS_ALLOW_METHODS = (
 
 CORS_ALLOW_HEADERS = (
     'accept',
-    
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 )
 
 ROOT_URLCONF = 'backend.urls'
